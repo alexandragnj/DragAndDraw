7bf9dcd9c3c42891ef72c7bc24ce8d2e8197cbd8
alexandragnj <102973575+alexandragnj@users.noreply.github.com>
2022-07-18 10:11:51 +0300

    implementing a custom view (#1)

--- app/src/main/java/com/example/draganddraw/DragAndDrawActivity.kt
+++ app/src/main/java/com/example/draganddraw/DragAndDrawActivity.kt
@@ -3,7 +3,7 @@ package com.example.draganddraw
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DragAndDrawActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
